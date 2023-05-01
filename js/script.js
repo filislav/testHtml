6db44aee728ff474da6d39994b4a696282207dfb
Вячеслав Филиппов <filislav43@gmail.com>
2023-05-01 17:02:42 +0400

    date & classes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-interval = setInterval(my_func,1000);
+let date = new Date();
+document.getElementById('date').innerHTML = `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`;
 
-let counter = 0;
-function my_func(){   
-    counter++;
-    console.log(`counter = ${counter}`);
-    if(counter==10){
-        clearInterval(interval);
+let arr = [9,8,2,3,5,7];
+let arr1 = arr.sort();
+console.log(arr1.join(","));
+
+class Person{
+    constructor(name,age,happyness){
+        this.name = name;
+        this.age = age;
+        this.happyness = happyness;
     }
+    
+    info() {
+        console.log(`${this.name},${this.age},${this.happyness}`); 
+    };
 }
 
-setTimeout(()=>{
-    alert("Работа программы завершена");
-},10000);
+let alex = new Person('Alex',19,true);
+let bob = new Person('Bob',20,false);
+alex.info();
+bob.info();
