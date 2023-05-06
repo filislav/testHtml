bcce4ae642a6b7232ae85aac466790ae867589a5
Вячеслав Филиппов <filislav43@gmail.com>
2023-05-06 18:02:31 +0400

    palindrom test

--- js/script.js
+++ js/script.js
@@ -1,39 +1,13 @@
-
-// function createIncrementor(n){
-//     return function(num){
-//         return n+num;
-//     }
-// }
-
-// const addOne = createIncrementor(1);
-// const addTen = createIncrementor(10);
-// console.log(addOne(3));
-// console.log(addTen(3));
-
-// function urlGenerator(dom){
-//     return function(url){
-//         return `https://${url}.${dom}`;
-//     }
-// }
-// let com = urlGenerator('com');
-// let ru = urlGenerator('ru');
-
-// console.log(com('microsoft'));
-// console.log(ru('yandex'));
-
-function logPerson(){
-    console.log(`Person : ${this.name}, ${this.age}, ${this.job}`);
+let form = document.getElementById('main-form');
+let result = document.getElementById('result');
+let isPolindrom = (event)=>{
+    event.preventDefault();
+    let input = form.polindrom.value;
+    input = input.toLowerCase();
+    result.innerHTML = `Результат : ${input===input.split("").reverse().join("")}`;   
+    return false;
 }
 
-const person1 = {name:"Alex",age:26,job:"engeneer"};
-const person2 = {name:"Elena",age:32,job:"cop"};
 
-function bind(context,fn){
-    return function(...args){
-        return fn.apply(context,args); //args пустой []0
-    }
-}
-
-bind(person1,logPerson)();
-bind(person2,logPerson)();
+form.addEventListener('submit',isPolindrom);
 
