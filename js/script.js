9d47266de30ede9bfc413bc15924850880ece871
Вячеслав Филиппов <filislav43@gmail.com>
2023-05-06 11:55:44 +0400

    zamyk

--- js/script.js
+++ js/script.js
@@ -1,35 +1,38 @@
-function hello(){
-    console.log('Hello',this);
+
+// function createIncrementor(n){
+//     return function(num){
+//         return n+num;
+//     }
+// }
+
+// const addOne = createIncrementor(1);
+// const addTen = createIncrementor(10);
+// console.log(addOne(3));
+// console.log(addTen(3));
+
+// function urlGenerator(dom){
+//     return function(url){
+//         return `https://${url}.${dom}`;
+//     }
+// }
+// let com = urlGenerator('com');
+// let ru = urlGenerator('ru');
+
+// console.log(com('microsoft'));
+// console.log(ru('yandex'));
+
+function logPerson(){
+    console.log(`Person : ${this.name}, ${this.age}, ${this.job}`);
 }
 
-const person = {
-    name:'Slava',
-    age:41,
-    sayHello: hello,
-    sayHelloWindow: hello.bind(window),
-    logInfo: function(job,phone){
-        console.group(`${this.name} info`);
-        console.log(`Name is ${this.name}`);
-        console.log(`Age is ${this.age}`);
-        console.log(`Job is ${job}`);
-        console.log(`Phone is ${phone}`);
-        console.groupEnd();
+const person1 = {name:"Alex",age:26,job:"engeneer"};
+const person2 = {name:"Elena",age:32,job:"cop"};
+
+function bind(context,fn){
+    return function(...args){
+        return fn.apply(context,args);
     }
 }
+bind(person1,logPerson)();
+bind(person2,logPerson)();
 
-const lena = {
-    name:"Lena",
-    age:35
-}
-// person.logInfo.bind(lena,"Frontend","8-888-999")();
-// person.logInfo.call(lena,"Frontend","8-888-999");
-// person.logInfo.apply(lena,["Frontend","8-888-999"]);
-
-let arr = [1,2,3,4,5];
-
-Array.prototype.mult = function(n){
-    return this.map((i)=>{
-        return i*n;
-    })
-}
-console.log(arr.mult(3));
