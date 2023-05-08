b83974532704cf0e7dc4389428d9db65353ccd4e
filislav <filislav43@gmail.com>
2023-05-08 10:53:59 +0400

    promises

--- js/script.js
+++ js/script.js
@@ -1,9 +1,64 @@
-let range = document.querySelector('.number');
-let rangeNum = document.querySelector('.range-num');
-rangeNum.innerHTML = 0;
-range.oninput = function (){
-    console.log(this.value);
-    rangeNum.style.left = (this.value - 20) +"px";
-    rangeNum.innerHTML = this.value;
-}
+console.log('Request data...');
 
+// setTimeout(()=>{
+//     console.log('Preparing data...');
+//     const backendData = {
+//         server:"aws",
+//         port:22334,
+//         status:"working"
+//     };
+
+//     setTimeout(()=>{
+//         backendData.modified = true;
+//         console.log('Data recieved:',backendData);
+//     },2000);
+
+// },2000);
+// const p = new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         console.log('Preparing data...');
+//         const backendData = {
+//         server:"aws",
+//         port:22334,
+//         status:"working"
+//         };
+//         resolve(backendData);
+//     },2000);
+// });
+
+
+// p.then((data)=>{
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             data.modified = true;
+//             //console.log('Data recieved:',data);
+//             resolve(data);
+//         },2000);        
+//     });    
+// })
+// .then((clientData)=>{
+//     clientData.fromPromise = true;
+//     return clientData;
+// })
+// .then((data)=>{
+//     console.log("Data recieved:",data);
+// })
+// .catch((err=>{
+//     console.log("Error:",err);
+// }))
+// .finally(()=>{
+//     console.log('finally');
+// })
+
+const sleep = ms => {return new Promise(resolve=>{setTimeout(()=>resolve(),ms)})};
+
+// timeout(2000).then(()=>{console.log('after 2 seconds')});
+// timeout(3000).then(()=>{console.log('after 3 seconds')});
+
+Promise.all([sleep(2000),sleep(3000)]).then(()=>{ //когда все промисы отработают
+    console.log('All promises');
+});
+
+Promise.race([sleep(2000),sleep(3000)]).then(()=>{ //когда первый промис отработает
+    console.log('Race promises');
+});
