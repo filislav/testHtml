135ea1a225e882b0dec19a9f2871e7487a5b0470
Вячеслав Филиппов <filislav43@gmail.com>
2023-06-20 12:44:52 +0400

    async await

--- js/script.js
+++ js/script.js
@@ -1,50 +1,32 @@
-// const person = Object.create({
-//     calculateAge(){
-//         console.log('Age:',new Date().getFullYear() - this.birthYear);
-//     }
-// },
-// {
-//     name:{
-//         value:'Slava',
-//         enumerable:true,
-//         writable:true,
-//         configurable:true,
-//     },
-//     birthYear:{
-//         value: 1982,
-//         enumerable:false,
-//         writable:true,
-//         configurable:false,
-//     },
-//     age:{
-//         get() {
-//             return new Date().getFullYear() - this.birthYear;
-//         },
-//         set(val) {
-//             //document.body.style.background ='red';
-//             this.birthYear = new Date().getFullYear() - val;
-//             console.log(this.birthYear);
-//         }
-//     }
-// });
+console.log("staring");
+const delay = ms => {
+    return new Promise(r => setTimeout(()=>r(),ms));
+};
 
-// const person = {
-//     name:'Slava',
-//     birthYear: 1982
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+async function fetchAsyncTodos(){ 
+    console.log('FetchTodo started');
+    try{
+        await delay(2000) // пока промис delay() не зарезолвится мы не перейдем к следующей строке кода. Это работа await
+        const responce = await fetch(url);
+        const data = await responce.json();
+        console.log('Data:',data);
+    }catch(e){
+        console.error(e);
+    }finally{
+        console.log('finally...');
+    }
+}
+
+fetchAsyncTodos(); //эта функция всегда возвращает нам промис последнего await и у нее так же можно вызвать метод then()
+// function fetchTodos(){
+//     console.log('FetchTodo started');
+//    return delay(2000)
+//    .then(()=>fetch(url)) // в ES6 если мы не пишем return то js автоматически возвращает нам результат
+//    .then(responce=>responce.json()); 
 // }
-// console.log(person);
-// for(let key in person){
-//     console.log('Key',key,person[key]);
-// }
-let arr = [0,0,1,1,1,2,2,2,2,3,3];
-let nArr = Array.from(new Set(arr));
-let sp = nArr.splice(1,2,0,0);
-let cars = [
-    {name:"Toyota",color:"Green"},
-    {name:"BMW",color:"Red"},
-    {name:"Mersedes",color:"Blue"}
-];
-let cars_names = Array.from(cars,({name})=>name);
-console.group("Result");
-console.log(cars_names);
-console.groupEnd();
+
+// fetchTodos().then(data => {
+//     console.log('Data:',data);
+// }).catch(e=>{console.error(e)});
